aede54ff44315f9802751bf6441553dcb00c096f
айдар сайфуллин <sajfullin870@gmail.com>
2022-11-24 11:26:53 +0500

    WQWQE

--- proverka_vetok/Reg.xaml.cs
+++ proverka_vetok/Reg.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
 namespace proverka_vetok
 {
     /// <summary>
@@ -21,8 +20,69 @@ namespace proverka_vetok
     {
         public Reg()
         {
-            InitializeComponent(); 
+            InitializeComponent();
         }
-       
+        int num = 0;
+        private void ChekTextBox_Click(object sender, RoutedEventArgs e)
+        {
+            var TSurname = Surname.Text;
+            var TName = Name.Text;
+            var TPatronymic = Patronymic.Text;
+            var TLogin = Login.Text;
+            var TPassword = Password.Text;
+            if (TSurname.Length == 0)
+            {
+                MessageBox.Show("Фамилия");
+                num++;
+            }
+            else
+            {
+            }
+            if (TName.Length == 0)
+            {
+                MessageBox.Show("Имя");
+                num++;
+            }
+            else
+            {
+            }
+            if (TPatronymic.Length == 0)
+            {
+                num++;
+                MessageBox.Show("Отчество");
+            }
+            else
+            {
+            }
+            if (TLogin.Length == 0)
+            {
+                MessageBox.Show("Логин");
+                num++;
+            }
+            else
+            {
+            }
+            if (TPassword.Length == 0)
+            {
+                MessageBox.Show("Пароль");
+                num++;
+            }
+            else
+            {
+            }
+            if (num > 0)
+            {
+                MessageBox.Show("Заполните пустые поля");
+                num = 0;
+            }
+            else
+            {
+                MessageBox.Show("Успешная регистрация");
+            }
+            if (TName == "Name")
+            {
+                MessageBox.Show("Error");
+            }
+        }
     }
 }
